67109ce61ddf7022afff9d3c959841d33505bdd7
Andre Yamin <andreyamin@gmail.com>
2018-07-19 10:54:57 +1200

    adding release number to file

--- version.php
+++ version.php
@@ -23,6 +23,7 @@
  */
 
 $plugin->component = 'block_event_tracker_graph';  
-$plugin->version = 2018071700; 
+$plugin->version = 2018071700;
+$plugin->release = '3.5';
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
