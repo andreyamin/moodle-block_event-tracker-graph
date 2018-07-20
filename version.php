03440e75fe1eef4a3c81dc1957b71a22187cdfb5
Andre Yamin <andreyamin@gmail.com>
2018-07-20 13:46:18 +1200

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_event_tracker_graph';
-$plugin->version = 2018071705;
+$plugin->version = 2018071706;
 $plugin->release = '3.6 beta';
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_BETA;
