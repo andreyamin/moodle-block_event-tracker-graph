cb0087251f8c2d8d4795693540103161fae79879
Andre Yamin <andreyamin@gmail.com>
2018-07-28 18:58:39 +1200

    Release 3.5.9

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_event_tracker_graph';
-$plugin->version = 2018071708;
-$plugin->release = '3.5.8';
+$plugin->version = 2018071709;
+$plugin->release = '3.5.9';
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
